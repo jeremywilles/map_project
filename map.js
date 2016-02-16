88d2c43076799e9b7f08ce77a3ed431738893d46
Jeremy Willes <jeremywilles@gmail.com>
2016-02-15 21:03:30 -0600

    map working

--- map.js
+++ map.js
@@ -1,1 +1,12 @@
 console.log("connected");
+
+//api key
+// AIzaSyDR_R0fIJ8eCx9SlRLLY5LtPuh9yTF4Oqc
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
