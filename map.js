72ce7b38726b227f5d51baf1ef13ab2f7f7f01fc
Jeremy Willes <jeremywilles@gmail.com>
2016-02-17 21:30:35 -0600

    added possible list filter, started lists

--- map.js
+++ map.js
@@ -14,8 +14,15 @@ function initMap() {
 //possible list library
 // http://www.listjs.com/
 
+//live search with knockout.js
+// http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+
 //todo
 //markers
+
+var locations = ["Animal Kingdom", "Epcot", "Magic Kingdom", "Hollywood Studios", "Disney Springs"];
+
+
 
 //listview
 
