a03c8404d60655227c4932fe2bec7b776016b789
Jeremy Willes <jeremywilles@gmail.com>
2016-03-31 22:19:33 -0500

    infoWindow works, todo: api info

--- map.js
+++ map.js
@@ -7,9 +7,11 @@
 
 //info windows with ajax
 
+
+
 //mobile site
 
-var map;
+var map, infoWindow;
 
 var gMarks = [];
 
@@ -26,7 +28,11 @@ function initMap() {
     zoom: 13
   });
   marks(markers);
-  }
+
+  addInfo();
+  launchInfo();
+
+}
 
 function marks(markers){
   for(var i = 0; i<markers.length; i++){
@@ -41,6 +47,9 @@ function marks(markers){
   	gMarks.push(marker);
 	}
 }
+	
+
+console.log('1'+gMarks);
 
 function clearOverlays(markersArray) {
 	console.log(markersArray);
@@ -49,6 +58,53 @@ function clearOverlays(markersArray) {
   }
   markersArray.length = 0;
 }
+
+//test input
+
+
+function addInfo(){
+ var contentString = '<div id="content">'+
+      '<div id="siteNotice">'+
+      '</div>'+
+      '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
+      '<div id="bodyContent">'+
+      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+      'sandstone rock formation in the southern part of the '+
+      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
+      'south west of the nearest large town, Alice Springs; 450&#160;km '+
+      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
+      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
+      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
+      'Aboriginal people of the area. It has many springs, waterholes, '+
+      'rock caves and ancient paintings. Uluru is listed as a World '+
+      'Heritage Site.</p>'+
+      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
+      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
+      '(last visited June 22, 2009).</p>'+
+      '</div>'+
+      '</div>';
+
+
+infoWindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
+}
+
+
+function launchInfo(){
+
+	for(var i = 0; i<gMarks.length; i++){
+		gMarks[i].addListener('click', function(){
+			infoWindow.open(map, this);
+		});
+	}
+}
+
+	
+
+
+
 
 
 
